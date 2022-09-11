444c50c0dd61782dd2c30972cc1111d73343983d
DIEGO ISRAEL SOUZA DOS SANTOS <diegodisouza10@gmail.com>
2022-09-11 01:16:20 -0300

    Upload

---
+++ js/scrooll.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal ({reset: true});
+sr.reveal('.banner_part .banner_text .banner_text_iner',{duration:3000});
+sr.reveal('.banner_part',{duration:2000});
+sr.reveal('.about_part .about_part_text h2',{duration:2000});
+sr.reveal('.section_tittle h2',{duration:2000});
+sr.reveal('.review_part .tour_pack_text h2',{duration:2000});
+sr.reveal('.footer-area .single-footer-widget h4',{duration:2000});
+sr.reveal('.footer-area .single-footer-widget p',{duration:3000});
+sr.reveal('.footer-area .single-footer-widget ul li',{duration:3000});
+sr.reveal('.footer-area .single-footer-widget ul li a',{duration:3000});
+
+
+
+
+
+
